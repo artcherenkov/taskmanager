2a5ba7e1ef21b1dc60944f79f89f9fc2bb762fbf
Артем Черенков <art.cherenkov@gmail.com>
2023-02-27 15:53:47 +0700

    убрал лишний код

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,5 @@ import { test, expect } from "@playwright/test";
 test("has title", async ({ page }) => {
   await page.goto("http://localhost:3000/");
 
-  const p = await page.getByText(/save to reload/i);
-
-  await expect(p).toBeVisible();
+  await expect(page.locator("p")).toHaveText(/save to reload/i);
 });
